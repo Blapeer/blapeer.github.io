2314338b8224048483aa8adcd9e4399296203a92
Blapeer <blapeerapp@gmail.com>
2024-05-30 16:27:34 -0600

    Add files via upload

---
+++ assets/js/conexion.js
@@ -0,0 +1,9 @@
+//var cn_ws = "https://Aetechnologies.somee.com/EGF/REQ.ashx?";
+
+//var cn_ws = "https://blapeer.somee.com/requestnoti.ashx?";
+
+//var cn_ws = "http://blapeer-001-site1.atempurl.com/requestnoti.ashx?";
+
+//var cn_ws = "https://amahcarev2.somee.com/REQ.ashx?";
+
+var cn_ws = "https://amahcarev2.somee.com/requestnoti.ashx?";
